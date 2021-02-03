7ecdf56f5bf8a88bf70743d14b35db5eb724f335
Mark Seliternikov <mark@DESKTOP-BE1MNRB.localdomain>
2021-02-03 17:34:46 +0200

    updated db and made a helper function

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 # connect to the database within the app folder
-conn = sqlite3.connect("gift-exchange.db")
+conn = sqlite3.connect("database.db")
 # the cursor for the database
 c = conn.cursor()
+
+print("test")
