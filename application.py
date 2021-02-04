3b76d42f012597640297b31221a0246c004312b6
Mark Seliternikov <mark@DESKTOP-BE1MNRB.localdomain>
2021-02-04 13:20:44 +0200

    major changes
    created email checking and registering in application.py, added a few templates, updated requirements, email checking was written in helper.py.

--- application.py
+++ application.py
@@ -4,6 +4,9 @@ from flask_session import Session
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
+# my helper func
+from helper import login_required, check_email
+
 # configure the a flask app
 app = FLASK(__name__)
 
@@ -29,52 +32,52 @@ conn = sqlite3.connect("database.db")
 # the cursor for the database
 c = conn.cursor()
 
-# the index route
+
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     """The main page (supposed to show only if logged in)"""
 
-## login route
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     """Log user in"""
 
-#     # Forget any user_id
-#     session.clear()
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
 
-#     # User reached route via POST (as by submitting a form via POST)
-#     if request.method == "POST":
+    # Forget any user_id
+    session.clear()
 
-#         # Ensure username was submitted
-#         if not request.form.get("username"):
-#             return apology("must provide username", 403)
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
 
-#         # Ensure password was submitted
-#         elif not request.form.get("password"):
-#             return apology("must provide password", 403)
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
 
-#         # Query database for username
-#         rows = c.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
 
-#         # Ensure username exists and password is correct
-#         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-#             return apology("invalid username and/or password", 403)
+        # Query database for username
+        rows = c.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
-#         # Remember which user has logged in
-#         session["user_id"] = rows[0]["id"]
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
 
-#         # Redirect user to home page
-#         return redirect("/")
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
 
-#     # User reached route via GET (as by clicking a link or via redirect)
-#     else:
-#         return render_template("login.html")
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 
-# logs user out
 @app.route("/logout")
 def logout():
+    """logs user out"""
 
     # Forget any user_id
     session.clear()
@@ -82,9 +85,62 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-## register route
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+
+    # if its a post method (send the server the register data)
+    if request.method == "POST":
+        # checks if username is empty
+        if request.form.get("username") == "":
+            flash("Must provide username")
+            return redirect("/register")
+
+        # Checks if username already exists
+        rows = c.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(rows) != 0:
+            flash(f"Sorry, {username} is already taken")
+            return redirect("/register")
+
+        # Checks if email is empty
+        if request.form.get("email") == "":
+            flash("Must provide email")
+            return redirect("/register")
+
+        # Checks if email is valid
+        if not check_email(request.form.get("email")):
+            flash("Email is invalid")
+            return redirect("/register")
+
+        # checks if password is empty
+        if request.form.get("password1") == "":
+            flash("Must provide password")
+            return redirect("/register")
+
+        # checks if user repeated the password
+        if request.form.get("password2") == "":
+            flash("Must repeat password")
+            return redirect("/register")
+
+        # chekcs if both passwords match
+        if request.form.get("password1") != request.form.get("password2"):
+            flash("Password and Repeat password do not match")
+            return redirect("/register")
+
+        # now that everything checks out that we can insert into the database
+        c.execute("INSERT INTO users (username, hash, email) VALUES (?, ?)", \
+            request.form.get("username"), \
+            generate_password_hash(request.form.get("password1")), \
+            request.form.get("email"))
+
+        # to improve user experience it logs in the user right after registering
+        rows = c.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        session["user_id"] = rows[0]["id"]
+        flash("You've registered successfully!")
+        return redirect("/")
+
+    # if its a get method (load the register page to register)
+    return render_template("register.html")
 
 
--- helper.py
+++ helper.py
@@ -1,10 +1,23 @@
-import requests
-import urllib.parse
-
 from flask import redirect, render_template, request, session
 from functools import wraps
+import re
 
-# ill add helper functions here to aid application.py
+"""
+Helping application.py operate
+"""
+
+def check_email(email):
+    """
+    Checks validitiy of email
+
+    https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
+    """
+    regex = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+
+    if (re.search(regex, email)):
+        return true
+    return false
+
 
 def login_required(f):
     """
@@ -15,6 +28,8 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
+            # makes sure the user is logged in before executing the
+            # decorated func
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
