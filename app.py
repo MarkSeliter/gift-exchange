e5efdc09f5cf2fa8648cbce9fe60de382241e5ba
Mark Seliternikov <mark.seliternikov@gmail.com>
2021-02-19 19:29:13 +0200

    some minor changes

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 
 # Ensure responses aren't cached
 @app.after_request
@@ -23,7 +25,6 @@ def after_request(response):
 
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
